51954a9b5978ce9e14f7b51d1f02028e984f423b
JohannesHupfeld <j.hupfeld@gmail.com>
2021-11-12 17:42:39 -0500

    moved initial getter functions from index.js to global.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,13 +1,3 @@
-const buttonDiv = () => document.getElementById("button-control")
-const buttonShow = () => document.getElementById("button-show")
-const buttonNew = () => document.getElementById("button-new")
-const ulDiv = () => document.getElementById("list")
-const ul = () => document.getElementById("categories-list")
-const productForm = () => document.getElementById("product-form")
-const productName = () => document.getElementById("product-name")
-const productPrice = () => document.getElementById("product-price")
-const productDescription = () => document.getElementById("product-description")
-const productSelectCategory = () => document.getElementById("category_id")
 
 document.addEventListener("DOMContentLoaded", () => {
     buttonShow().addEventListener("click", handleClick)
